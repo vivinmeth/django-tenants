47f11795a964c22a761a2f50fc808376c379d219
VivinMeth <vivinmeth@surkyl.com>
2024-08-09 21:09:53 +0530

    Update setup.py to allow django 5.1 for testing

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     install_requires=[
-        'Django >=2.1,<5.1',
+        'Django >=2.1,<=5.1',
     ],
     zip_safe=False,
 )
